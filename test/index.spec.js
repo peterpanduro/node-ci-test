ff456d6bda7eb029ccfc04357cd447ed8bd886db
Peter Panduro <peter@kodifiera.se>
2020-04-16 15:55:57 +0200

    Fixed alot of babel and testing stuff

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,5 @@
-var request = require('supertest');
+//var request = require('supertest');
+import request from 'supertest';
 var app = require('../src/index.js');describe('GET /', function() {
  it('respond with hello world', function(done) { //navigate to root and check the the response is "hello world"
  request(app).get('/').expect('hello world', done);
