8221c4e79be267dd74d237651738b990d7b86163
Peter Panduro <peter@kodifiera.se>
2020-04-24 11:29:00 +0200

    Returns hello worlds

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,14 +1,15 @@
-import chai, {expect} from 'chai';
-import chaiHttp from 'chai-http';
-import app from '../src/index'
+import chai, { expect } from "chai";
+import chaiHttp from "chai-http";
+import app from "../src/index";
 chai.use(chaiHttp);
 
-it('responds with hello world', done => {
-    chai.request(app)
-        .get('/')
-        .end((err ,res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(200);
-            done();
-        })
-})
+it("responds with status code 200", (done) => {
+	chai
+		.request(app)
+		.get("/")
+		.end((err, res) => {
+			expect(err).to.be.null;
+			expect(res).to.have.status(200);
+			done();
+		});
+});
